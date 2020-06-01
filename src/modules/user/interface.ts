786d78f6d5950cfa9e44d3d1535eb58d20256a09
hackagon <phonghiavan@gmail.com>
2020-06-01 22:51:00 +0700

    user controller

--- src/modules/user/interface.ts
+++ src/modules/user/interface.ts
@@ -1,6 +1,17 @@
-export interface User {
+export interface User extends UserDTO {
   id: number;
   email: string;
   password: string;
   fullName: string;
+}
+
+export interface UserDTO {
+  email: string;
+  password: string;
+  fullName: string;
+}
+
+export interface Credentials {
+  email: string;
+  password: string;
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Body, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
 import { User } from "./user.entity";
+import { UserDTO, Credentials } from "./interface";
 
 @Injectable()
 export class UserService {
@@ -11,5 +12,28 @@ export class UserService {
 
   async getUsers(): Promise<User[]> {
     return await this.userRepository.find();
+  }
+
+  async createUser(@Body() userDTO: UserDTO): Promise<User> {
+    const newUser = this.userRepository.create(userDTO);
+    return await newUser.save();
+  }
+
+  async updateUser(id: number, userDTO: Omit<UserDTO, "email" | "password">): Promise<User> {
+    let foundUser = await this.userRepository.findOne(id);
+    if (!foundUser) throw new NotFoundException("User not found");
+
+    foundUser = Object.assign(foundUser, userDTO, userDTO, {})
+    await foundUser.save();
+
+    return foundUser;
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const foundUser = await this.userRepository.findOne(id);
+    if (!foundUser) throw new NotFoundException("User not found");
+
+    await this.userRepository.delete(id);
+    return foundUser;
   }
 }
