b70c3350ea391f80e27241968362001f04a1cba7
hackagon <phonghiavan@gmail.com>
2020-06-03 15:10:15 +0700

    clear

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe, BadRequestException, UnprocessableEntityException, ArgumentMetadata } from "@nestjs/common";
 import { ValidationError, useContainer } from 'class-validator';
 
-export class ValidationPipe422 extends ValidationPipe {
-  public async transform(value, metadata: ArgumentMetadata) {
-    try {
-      return await super.transform(value, metadata)
-    } catch (e) {
-      if (e instanceof BadRequestException) {
-        console.log(e)
-        throw new UnprocessableEntityException(e)
-      }
-    }
-  }
-}
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api");
