bf4aebb32f7dcc8b684d4bffaad39b50e6b0cbfe
hackagon <phonghiavan@gmail.com>
2020-06-04 10:16:36 +0700

    clean code

--- src/decorator/isUnique.decorator.ts
+++ src/decorator/isUnique.decorator.ts
@@ -1,37 +1,5 @@
-// import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
 import { InjectRepository, InjectEntityManager } from '@nestjs/typeorm';
 import { UserRepository } from '../modules/user/user.repository';
-// import { Repository, EntityManager } from 'typeorm';
-// import { Injectable } from '@nestjs/common';
-
-// export function isUnique(validationOptions?: ValidationOptions) {
-//   return (object: any, propertyName: string) => {
-//     registerDecorator({
-//       target: object.constructor,
-//       propertyName,
-//       options: validationOptions,
-//       constraints: [],
-//       validator: IsUniqueConstraint,
-//     });
-//   };
-// }
-
-// @ValidatorConstraint({ name: 'isUnique', async: true })
-// @Injectable()
-// export class IsUniqueConstraint implements ValidatorConstraintInterface {
-//   constructor(
-//     @InjectRepository(UserRepository) private userRepository: UserRepository,
-//   ) {
-
-//   }
-
-//   async validate(email: string, args: string) {
-//     console.log("args: ", args);
-//     const foundUser = await this.userRepository.findOne({ email })
-//     if (foundUser) return false;
-//     return true
-//   }
-// }
 
 import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from "class-validator";
 import { Injectable } from '@nestjs/common';
@@ -49,15 +17,3 @@ export class IsUniqueEmail implements ValidatorConstraintInterface {
     });
   }
 }
-
-// export function isUnique(validationOptions?: ValidationOptions) {
-//   return function (object: Object, propertyName: string) {
-//     registerDecorator({
-//       target: object.constructor,
-//       propertyName: propertyName,
-//       options: validationOptions,
-//       constraints: [],
-//       validator: IsUniqueEmail
-//     });
-//   };
-// }
