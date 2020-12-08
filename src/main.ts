57826b74dc96b21aafddc389cae271a47b4c40ff
Hackagon <phonghiavan@gmail.com>
2020-12-08 16:15:48 +0700

    sql script for init actionType records

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, BadRequestException, INestApplication } from '@nestjs/common';
+import {
+  ValidationPipe,
+  BadRequestException,
+  INestApplication,
+} from '@nestjs/common';
 import { ValidationError, useContainer } from 'class-validator';
-import * as datasource from "./config/datasource.config.json";
+import * as datasource from './config/datasource.config.json';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule, {
-    logger: ["warn", "error"]
-  })
-  app.setGlobalPrefix("/api");
+    logger: ['warn', 'error'],
+  });
+  app.setGlobalPrefix('/api');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,19 +20,22 @@ async function bootstrap() {
         return new BadRequestException(validationErrors);
       },
       validationError: {
-        target: false
-      }
-    }));
+        target: false,
+      },
+    }),
+  );
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  const port = process.env.PORT || 5000
+  const port = process.env.PORT || 5001;
 
   await app.listen(port, () => {
-    console.log(`====================== Server is running ======================`)
-    console.log(`Port: ${port}`)
-    console.log(`Environment: ${process.env.NODE_ENV}`)
-    console.log(`Database: ${datasource[process.env.NODE_ENV].host}`)
+    console.log(
+      `====================== Server is running ======================`,
+    );
+    console.log(`Port: ${port}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log(`Database: ${datasource[process.env.NODE_ENV].host}`);
   });
 }
 bootstrap();
--- src/modules/itemType/itemType.service.ts
+++ src/modules/itemType/itemType.service.ts
@@ -1,42 +1,47 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { ItemTypeRepository } from "./itemType.repository";
-import { ItemType } from "./itemType.entity";
-import { getConnection } from "typeorm"
-import * as path from "path";
-import * as fs from "fs";
-import * as _ from "lodash"
-import * as BBPromise from "bluebird"
-import { clearConfigCache } from "prettier";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ItemTypeRepository } from './itemType.repository';
+import { ItemType } from './itemType.entity';
+import { getConnection } from 'typeorm';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as _ from 'lodash';
+import * as BBPromise from 'bluebird';
+import { clearConfigCache } from 'prettier';
 
 @Injectable()
 export class ItemTypeService implements OnModuleInit {
   constructor(
-    @InjectRepository(ItemTypeRepository) private itemTypeRepository: ItemTypeRepository
-  ) { }
+    @InjectRepository(ItemTypeRepository)
+    private itemTypeRepository: ItemTypeRepository,
+  ) {}
 
   // https://stackoverflow.com/questions/22636388/import-sql-file-in-node-js-and-execute-against-postgresql
   async onModuleInit() {
-    const sqlPath = path.join(__dirname, "../../../sql/init_item_type_records.sql")
+    console.log('run');
+    const sqlPath = path.join(
+      __dirname,
+      '../../../sql/init_item_type_records.sql',
+    );
 
     const queries = _.chain(sqlPath)
       .thru(val => {
-        return fs.readFileSync(val, { encoding: "utf8" }).toString()
+        return fs.readFileSync(val, { encoding: 'utf8' }).toString();
       })
-      .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
+      .replace(/(\r\n|\n|\r)/gm, ' ') // remove newlines
       .replace(/\s+/g, ' ') // excess white space
-      .split(";")
+      .split(';')
       .filter()
-      .value()
+      .value();
 
-    await BBPromise.map(queries, sqlQuery => getConnection().query(sqlQuery))
+    await BBPromise.map(queries, sqlQuery => getConnection().query(sqlQuery));
   }
 
   async getItemTypes(): Promise<ItemType[]> {
-    return await this.itemTypeRepository.find()
+    return await this.itemTypeRepository.find();
   }
 
   async getItemTypeById(id: number): Promise<ItemType> {
-    return await this.itemTypeRepository.findOne(id)
+    return await this.itemTypeRepository.findOne(id);
   }
 }
